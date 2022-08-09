d82cd24fe3806840ae610a929035a2a0af95919e
Juan J Guirao Garcia <juan.jose.guirao@gmail.com>
2022-08-09 07:53:57 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PlainForm from './Components/PlainForm';
+
+function App() {
+    return (
+        <div className='App'>
+            <PlainForm />
+        </div>
+    );
+}
+
+export default App;
